8470ca64b3b3407e530642983d1da3f7540f6ab0
steviejamison <jpatstevenson@gmail.com>
2019-02-28 15:12:34 +0100

    Outline of first 2 methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("test");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        String file = "hamlet.xml";
+        Document document = builder.parse(new File(file));
+
+        //Extract root
+        Element root = document.getDocumentElement();
+
+        //Find number of personae
+        NodeList personaList = root.getElementsByTagName("PERSONA");
+        int numPersona = personaList.getLength();
+        System.out.println ("There are " + numPersona + " persona");
+
+        //Find number of times a character speaks
+        String speakerName = "HAMLET";
+        NodeList speakerList = root.getElementsByTagName("SPEAKER");
+        int numOfSpeaker = 0;
+
+        for(Node speaker: speakerList)
+        {
+            numOfSpeaker++;
+        }
     }
 }
