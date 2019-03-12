8bb162535ac1c29326163a183bcd8047742148d5
steviejamison <jpatstevenson@gmail.com>
2019-03-12 22:31:52 +0100

    Third edition

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import org.xml.sax.XMLReader;
 
 import javax.xml.parsers.*;
 import java.io.*;
+
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -38,20 +41,33 @@ public class Main {
             }
             System.out.println (checkedName + " speaks " + count + " times ");
 
-            //Find line of text from uer input
+            //Find line of text from user input
+            //default of "To be, or not to be"?
             String fragment = input.nextLine();
+            boolean isFound = false;
             NodeList lineList = root.getElementsByTagName("LINE");
             Element lineElement;
             Node lineNode;
+
+            long lStartTime = System.nanoTime();
             for (int i = 0; i < lineList.getLength(); i++)
             {
                 lineNode = lineList.item(i);
                 lineElement = (Element) lineNode;
 
-                if (lineElement.getTextContent().contains(fragment))
-                    System.out.println(lineElement.getTextContent());
+                if (lineElement.getTextContent().contains(fragment)) {
+                    System.out.println("The fragment has been found in the following sentence:");
+                    System.out.println("'" + lineElement.getTextContent() + "'");
+                    isFound = true;
+                }
             }
-
+            long lEndTime = System.nanoTime();
+            if (!isFound)
+                System.out.println("Sorry, fragment not found.");
+            System.out.println("Search performed in " + ((lEndTime - lStartTime)/1000000000.0) + " seconds");
+            if (isFound) {
+                System.out.println("Do you want to replace it? (Y/N)");
+            }
         }
         catch (ParserConfigurationException e) {
             e.printStackTrace();
